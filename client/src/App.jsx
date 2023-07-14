b62fa0f8db25924df953885261566eac3cddd411
RohitP2005 <rohitthevar@gmail.com>
2023-07-14 22:42:07 +0530

    Styled Login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Login from "./components/Login";
+import Login from "./components/login/Login";
 import axios from "axios";
 import { Route, Routes } from "react-router-dom";
 
