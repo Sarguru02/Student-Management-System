753556cd4c640ea26485850e6469723b1ab07a8a
RohitP2005 <rohitthevar@gmail.com>
2023-07-21 13:36:48 +0530

    teacher page styled

--- client/src/components/TeacherLandingPage/Teacher.jsx
+++ client/src/components/TeacherLandingPage/Teacher.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
+import "./teacher.css";
 
 function Teacher() {
   const [file, setFile] = useState();
@@ -19,18 +20,28 @@ function Teacher() {
     axios.post("/upload", formData);
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="marks">Choose Excel file for marks: </label>
+    <div className="container-fluid">
+      <div className="form">
+    <form onSubmit={handleSubmit} className="form-control">
+      <div className="text-success p-3">
+        <h2>Upload your File</h2>
+      </div>
+      <label className="inp-lab" htmlFor="marks">
+      {/* <i className=".material-symbols-outlined">Icon </i> */}
+      </label>
+
       <input
+        className="icon-input"
         type="file"
         name="marksExcel"
         id="marks"
         accept=".xlsx"
         onChange={(e) => setFile(e.target.files[0])}
-      />
-
-      <button type="submit">Submit form</button>
+        />
+      <button type="submit" className="btn btn-outline-success">Submit form</button>
     </form>
+        </div>
+        </div>
   );
 }
 
