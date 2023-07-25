893ef1df9aff1b2dd778e032ea9a35a569ce2789
Sarguru02 <psgnhealth@gmail.com>
2023-07-25 22:01:27 +0530

    add schoolname

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ import { Route, Routes } from "react-router-dom";
 import Teacher from "./components/TeacherLandingPage/Teacher";
 import { AuthProvider } from "./contexts/AuthContext";
 import Student from "./components/StudentLandingPage/Student";
+import Navbar from "./components/Navbar/Navbar";
 
 axios.defaults.baseURL = "https://student-management-system-inky.vercel.app/";
 
 function App() {
   return (
     <AuthProvider>
+      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/teacher" element={<Teacher />} />
